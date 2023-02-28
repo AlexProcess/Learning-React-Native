a19644bf6f3a6fdd4b6cd7572ac2ca4c81d61c00
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-02-28 20:11:28 +0100

    funciones, retornos y argumentos ts

--- reforzamiento/src/App.tsx
+++ reforzamiento/src/App.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
-import { TiposBasicos } from "./TiposBasicos";
-import { ObjetosLiterales } from "./ObjetosLiterales";
+import { TiposBasicos } from "./Ts/TiposBasicos";
+import { ObjetosLiterales } from "./Ts/ObjetosLiterales";
+import { Funciones } from "./Ts/Funciones";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <TiposBasicos /> */}
-      <ObjetosLiterales />
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* <TiposBasicos /> */}
+            <ObjetosLiterales />
+            <Funciones />
+        </div>
+    );
 }
 
 export default App;
