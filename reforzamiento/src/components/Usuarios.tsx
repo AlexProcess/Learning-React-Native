bbb69e96300e4de3f1849a1493ee8d062f62567e
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-03-02 18:42:17 +0100

    Mostrar usuarios en pantalla

--- reforzamiento/src/components/Usuarios.tsx
+++ reforzamiento/src/components/Usuarios.tsx
@@ -1,21 +1,51 @@
+import { renderHook } from "@testing-library/react";
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { reqResApi } from "../api/reqResApi";
 import { ReqResListado, Usuario } from "../interfaces/reqRes";
 
 export const Usuarios = () => {
-    const [usuario, setUsuarios] = useState<Usuario>([]);
+    const [usuarios, setUsuarios] = useState<Usuario[]>([]);
 
     useEffect(() => {
         // llamar al API
         reqResApi
             .get<ReqResListado>("/users")
             .then((resp) => {
-                console.log(resp.data.data[0].last_name);
+                console.log(resp.data.data);
+                setUsuarios(resp.data.data);
             })
             .catch(console.log);
     }, []);
 
+    const renderItem = ({
+        id,
+        first_name,
+        last_name,
+        email,
+        avatar,
+    }: Usuario) => {
+        return (
+            <>
+                <tr key={id.toString()}>
+                    <td>
+                        <img
+                            src={avatar}
+                            alt={first_name}
+                            style={{
+                                width: 50,
+                                borderRadius: 100,
+                            }}
+                        ></img>
+                    </td>
+                    <td>
+                        {first_name} {last_name}
+                    </td>
+                    <td>{email}</td>
+                </tr>
+            </>
+        );
+    };
     return (
         <>
             <h1>Usuarios</h1>
@@ -27,7 +57,7 @@ export const Usuarios = () => {
                         <th>Email</th>
                     </tr>
                 </thead>
-                <tbody></tbody>
+                <tbody> {usuarios.map(renderItem)} </tbody>
             </table>
         </>
     );
